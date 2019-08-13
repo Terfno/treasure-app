8cea623d0233e3ab292adea9117dc31ed76b49ed
terfno <sueda.takahito@gmail.com>
2019-08-13 11:05:48 +0900

    fix repo article.go user_id

--- backend/repository/article.go
+++ backend/repository/article.go
@@ -27,13 +27,13 @@ SELECT id, user_id, title, body FROM article WHERE id = ?
 
 func CreateArticle(db *sqlx.Tx, a *model.Article) (sql.Result, error) {
 	stmt, err := db.Prepare(`
-INSERT INTO article (user_id, title, body) VALUES (?, ?)
+INSERT INTO article (title, body, user_id) VALUES (?, ?, ?)
 `)
 	if err != nil {
 		return nil, err
 	}
 	defer stmt.Close()
-	return stmt.Exec(a.Title, a.Body)
+	return stmt.Exec(a.Title, a.Body, a.User_ID)
 }
 
 func UpdateArticle(db *sqlx.Tx, id int64, a *model.Article) (sql.Result, error) {
