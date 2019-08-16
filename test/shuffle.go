208a744029fb0a0ee7f9813ef8aeec8e6c9765cc
terfno <sueda.takahito@gmail.com>
2019-08-16 14:24:57 +0900

    :zap: いい感じにフォーマットしていい感じにした

--- test/shuffle.go
+++ test/shuffle.go
@@ -1,33 +1,36 @@
 package main
 
 import (
-    "fmt"
-		"math/rand"
-		"time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-    n := 30
-    arr := generate(n)
+	n := 30
+	arr := generate(n)
 
-    fmt.Println(arr)
-    shuffle(arr)
-    fmt.Println(arr)
+	fmt.Println(shuffle(arr))
 }
 
 func generate(n int) []string {
-    arr := make([]string, n)
-    for i := range arr {
-        arr[i] = fmt.Sprintf("%02d", i)
-    }
-    return arr
+	arr := make([]string, n)
+
+	for i := range arr {
+		arr[i] = fmt.Sprintf("%02d", i)
+	}
+
+	return arr
 }
 
-func shuffle(data []string) {
-		n := len(data)
-		rand.Seed(time.Now().UnixNano())
-    for i := n - 1; i >= 0; i-- {
-        j := rand.Intn(i + 1)
-        data[i], data[j] = data[j], data[i]
-    }
+func shuffle(data []string) []string {
+	n := len(data)
+	rand.Seed(time.Now().UnixNano())
+
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		data[i], data[j] = data[j], data[i]
+	}
+
+	return data
 }
