45958b46aea8b7b4fe593cb5dba3c91e9ab94129
terfno <sueda.takahito@gmail.com>
2019-08-16 13:57:14 +0900

    :bug: migrate/upでミスったので修正。多分イケてる

--- database/migrations/1_create_list.sql
+++ database/migrations/1_create_list.sql
@@ -1,9 +1,8 @@
 -- +goose Up
 CREATE TABLE list(
   id int(2) UNSIGNED NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY `id` (`id`)
+  name VARCHAR(200) NOT NULL,
+  PRIMARY KEY (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --  + goose Down
--- database/migrations/2_create_log.sql
+++ database/migrations/2_create_log.sql
@@ -2,9 +2,7 @@
 CREATE TABLE log (
   date DATETIME NOT NULL,
   map JSON NOT NULL,
-  PRIMARY KEY (date),
-  UNIQUE KEY `date` (`date`),
-  UNIQUE KEY `map` (`map`)
+  PRIMARY KEY (date)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- +goose Down
