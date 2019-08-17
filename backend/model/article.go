3e74ca4f6cc7fe56d8207a940550663b54a7446d
terfno <sueda.takahito@gmail.com>
2019-08-18 00:39:07 +0900

    :bug: organize dir

--- backend/model/article.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Article struct {
-	ID    int64  `db:"id" json:"id"`
-	Title string `db:"title" json:"title"`
-	Body  string `db:"body" json:"body"`
-	User_ID int64 `db:"user_id" json:"user_id"`
-}
