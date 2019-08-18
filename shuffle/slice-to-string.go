d8f5e636c106b5cee626d766a85ee8ec2158bf0f
terfno <sueda.takahito@gmail.com>
2019-08-19 00:46:12 +0900

    :zap: update slice to string | 実装が悪いけど動きそうだからおｋ

--- shuffle/slice-to-string.go
+++ shuffle/slice-to-string.go
@@ -2,8 +2,17 @@ package shuffle
 
 func SliceToString(slice []string) string {
 	var str string
-	for _, v := range slice {
-		str += v + ", "
+	length := len(slice)
+
+	str += "["
+	for index, v := range slice {
+		if index != length {
+			str += v + " "
+		} else {
+			str += v
+		}
 	}
+	str += "]"
+
 	return str
 }
